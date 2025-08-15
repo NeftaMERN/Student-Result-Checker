6a0cd6d39848989d9da4f4030a2bc93df6d926a0
Neftalem <dneftalem@gmail.com>
2025-08-15 17:14:21 +0300

    finshed the chaker

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 import os 
 
 Fill = "score.txt"
+
+def chake_resalt():
+
+    if not os.path.exists(Fill):
+        print("Your data is not alone")
+        return
+
+    student_id = input("Enter Your ID: ")
+    name = input("Enter Your Name: ")
+
+    
+    with open(Fill, 'r') as f:
+        lines = f.readlines()
+
+    found = False
+    for line in lines:
+        
+        record_id, record_name, scores, avg = line.strip().split(':')
+
+        if record_id == student_id and record_name.lower() == name.lower():
+            print( f"\n Hello {record_name}")
+            print(f"Your Score: {scores}")
+            print(f"Your Average: {avg}")
+            found = True
+            break
+
+    if not found:
+        print(" No matching student found. Check your ID and name!")    
+
+chake_resalt()
